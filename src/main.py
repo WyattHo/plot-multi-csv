798948bac5c927c714326aad3fc27e90ec107d1b
WyattHo <wyatthoho@gmail.com>
2023-08-17 10:25:24 +0800

    simplify

--- src/main.py
+++ src/main.py
@@ -4,39 +4,29 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-this_dir = os.path.dirname(__file__)
-csv_directory = os.path.join(this_dir, os.pardir, 'data\\output')
+# assign directory
+csv_directory = 'D:\\my-analysis\\kubota-package-7-module\\data\\output\\freq_1_200\\modal_linear_50'
 csv_names = os.listdir(csv_directory)
 
-fig_subtitles = ['Contact', 'Tie', 'Tie_Screw']
+
+# initial the figure
+fig = plt.figure(figsize=(4.8, 2.4), tight_layout=True)
+ax = plt.axes()
+
+
+# plot data
 labels = ['excite-x', 'excite-y', 'excite-z']
-csv_prenames =[
-    'xm25_hks_frf_maximum_stress_subcase',
-    'xm25_hks_frf_tie_maximum_stress_subcase',
-    'xm25_hks_frf_tie_small_maximum_stress_subcase'
-]
-
-
-csv_name_init, fig_idx, label_idx = 'init', 0, 0
-for name in csv_names:
-    if label_idx == 0:
-        fig = plt.figure(figsize=(4.8, 2.4), tight_layout=True)
-        ax = plt.axes()
-
+for label_idx, name in enumerate(csv_names):
     path = os.path.join(csv_directory, name)
     df = pd.read_csv(path)
-    ax.plot(df['frequency'], df['max_stress'], label=labels[label_idx])
+    ax.semilogx(df['frequency'], df['max_stress'], label=labels[label_idx])
 
-    if label_idx == 2:
-        subtitle = fig_subtitles[fig_idx]
-        ax.set_title(f'Maximum Stress Distribution - {subtitle}')
-        ax.set_ylabel('Stress, MPa')
-        ax.set_xlabel('Frequency, Hz')
-        ax.set_xlim([0, 200])
-        ax.grid(visible=True, axis='both')
-        ax.legend()
-        plt.show()
-        label_idx = 0
-        fig_idx += 1
-    else:
-        label_idx += 1
+
+# figure settings
+ax.set_title(f'Maximum Stress Distribution')
+ax.set_ylabel('Stress, MPa')
+ax.set_xlabel('Frequency, Hz')
+ax.set_xlim([10, 200])
+ax.grid(visible=True, axis='both')
+ax.legend()
+plt.show()
