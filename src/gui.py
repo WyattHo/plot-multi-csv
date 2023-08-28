048bfa0224195ee447c91932b62b749307fa7168
WyattHo <wyatthoho@gmail.com>
2023-08-28 17:41:27 +0800

    refactor

--- src/gui.py
+++ src/gui.py
@@ -66,54 +66,54 @@ def clear():
     ...
 
 
-def main():
+
+def initial_main_window() -> tk.Tk:
     root = tk.Tk()
     root.title('PlotCSV')
     root.resizable(width=0, height=0)
     root.configure()
+    return root
 
-    font_label = font.Font(family='Helvetica', size=10)
-    font_btn = font.Font(family='Helvetica', size=10)
-    
-    # frame up
-    frame_up = tk.LabelFrame(root, text='Choose the csv file')
-    frame_up.grid(row=0, column=0, padx=5, pady=5, ipadx=1, ipady=1, sticky=tk.W)
-    frame_up['font'] = font_label
-    
-    frame_up_up = tk.Frame(frame_up)
-    frame_up_up.grid(row=0, column=0)
 
-    frame_up_right = tk.Frame(frame_up)
-    frame_up_right.grid(row=0, column=1, rowspan=2)
+def create_directory_frame(root: tk.Tk, font_label: font.Font, font_btn: font.Font) -> tk.StringVar:
+    frame = tk.LabelFrame(root, text='Choose the csv file')
+    frame.grid(row=0, column=0, padx=5, pady=5, ipadx=1, ipady=1, sticky=tk.W)
+    frame['font'] = font_label
+    
+    subframe_left = tk.Frame(frame)
+    subframe_left.grid(row=0, column=0)
+    subframe_right = tk.Frame(frame)
+    subframe_right.grid(row=0, column=1, rowspan=2)
 
     stringvar = tk.StringVar()
-    tgtdir_entry = tk.Entry(frame_up_up, width=50, textvariable=stringvar)
+    tgtdir_entry = tk.Entry(subframe_left, width=50, textvariable=stringvar)
     tgtdir_entry.grid(row=0, column=0, padx=5, pady=5)
 
-    choose_btn = tk.Button(frame_up_right, text='Choose', command=lambda: open_dir(stringvar), width=6)
+    choose_btn = tk.Button(subframe_right, text='Choose', command=lambda: open_dir(stringvar), width=6)
     choose_btn.grid(row=0, column=0, padx=5, pady=5, ipadx=1, ipady=1)
     choose_btn['font'] = font_btn
+    return stringvar
 
 
-    # frame down
-    frame_dw = tk.LabelFrame(root, text='Working area')
-    frame_dw.grid(row=1, column=0, padx=5, pady=5)
-    frame_dw.propagate(0)
-    frame_dw['font'] = font_label
+def create_working_frame(root: tk.Tk, font_label: font.Font, font_btn: font.Font, stringvar: tk.StringVar):
+    frame = tk.LabelFrame(root, text='Working area')
+    frame.grid(row=1, column=0, padx=5, pady=5)
+    frame.propagate(0)
+    frame['font'] = font_label
     
     # frame down left 1
-    frame_dw_left_1 = tk.Frame(frame_dw, width=250, height=300)
-    frame_dw_left_1.grid(row=0, column=0, padx=5, pady=5)
-    frame_dw_left_1.propagate(0)
+    subframe_left_1 = tk.Frame(frame, width=250, height=300)
+    subframe_left_1.grid(row=0, column=0, padx=5, pady=5)
+    subframe_left_1.propagate(0)
 
-    scrollbar_ver_left = tk.Scrollbar(frame_dw_left_1)
+    scrollbar_ver_left = tk.Scrollbar(subframe_left_1)
     scrollbar_ver_left.pack(side=tk.RIGHT, fill=tk.Y)
 
-    scrollbar_hor_left = tk.Scrollbar(frame_dw_left_1, orient='horizontal')
+    scrollbar_hor_left = tk.Scrollbar(subframe_left_1, orient='horizontal')
     scrollbar_hor_left.pack(side=tk.BOTTOM, fill=tk.X)
 
     treeview_read = ttk.Treeview(
-        frame_dw_left_1, 
+        subframe_left_1, 
         yscrollcommand=scrollbar_ver_left.set, 
         xscrollcommand=scrollbar_hor_left.set,
         height=15
@@ -131,12 +131,12 @@ def main():
     treeview_read.heading('1', text='')
     
     # frame down left 2
-    frame_dw_left_2 = tk.Frame(frame_dw)
-    frame_dw_left_2.grid(row=1, column=0)
-    frame_dw_left_2.propagate(0)
+    subframe_left_2 = tk.Frame(frame)
+    subframe_left_2.grid(row=1, column=0)
+    subframe_left_2.propagate(0)
 
     read_btn = tk.Button(
-        frame_dw_left_2, 
+        subframe_left_2, 
         text='Read', 
         command=lambda: read_csv(treeview_read, stringvar, scrollbar_ver_left, scrollbar_hor_left), 
         width=6
@@ -145,18 +145,18 @@ def main():
     read_btn['font'] = font_btn
 
     # frame down right 1
-    frame_dw_right_1 = tk.Frame(frame_dw, width=250, height=300)
-    frame_dw_right_1.grid(row=0, column=1, padx=5, pady=5)
-    frame_dw_right_1.propagate(0)
+    subframe_right_1 = tk.Frame(frame, width=250, height=300)
+    subframe_right_1.grid(row=0, column=1, padx=5, pady=5)
+    subframe_right_1.propagate(0)
 
-    scrollbar_ver_right = tk.Scrollbar(frame_dw_right_1)
+    scrollbar_ver_right = tk.Scrollbar(subframe_right_1)
     scrollbar_ver_right.pack(side=tk.RIGHT, fill=tk.Y)
 
-    scrollbar_hor_right = tk.Scrollbar(frame_dw_right_1, orient='horizontal')
+    scrollbar_hor_right = tk.Scrollbar(subframe_right_1, orient='horizontal')
     scrollbar_hor_right.pack(side=tk.BOTTOM, fill=tk.X)
 
     treeview_draw = ttk.Treeview(
-        frame_dw_right_1, 
+        subframe_right_1, 
         yscrollcommand=scrollbar_ver_right.set, 
         xscrollcommand=scrollbar_hor_right.set,
         height=15
@@ -174,19 +174,24 @@ def main():
     treeview_draw.heading('1', text='')
 
     # frame down right 2
-    frame_dw_right_2 = tk.Frame(frame_dw)
-    frame_dw_right_2.grid(row=1, column=1)
+    subframe_right_2 = tk.Frame(frame)
+    subframe_right_2.grid(row=1, column=1)
 
-    used_indices = []
-    clear_state = False
-    draw_btn = tk.Button(frame_dw_right_2, text='Draw', command=lambda: draw(), width=6)
+    draw_btn = tk.Button(subframe_right_2, text='Draw', command=lambda: draw(), width=6)
     draw_btn.grid(row=0, column=0, padx=5, pady=5, ipadx=1, ipady=1)
     draw_btn['font'] = font_btn
     
-    clear_btn = tk.Button(frame_dw_right_2, text='Clear', command=lambda: clear(), width=6)
+    clear_btn = tk.Button(subframe_right_2, text='Clear', command=lambda: clear(), width=6)
     clear_btn.grid(row=0, column=1, padx=5, pady=5, ipadx=1, ipady=1)
     clear_btn['font'] = font_btn
-    
+
+
+def main():
+    root = initial_main_window()
+    font_label = font.Font(family='Helvetica', size=10)
+    font_btn = font.Font(family='Helvetica', size=10)
+    stringvar = create_directory_frame(root, font_label, font_btn)
+    create_working_frame(root, font_label, font_btn, stringvar)
     root.mainloop()
     
 
