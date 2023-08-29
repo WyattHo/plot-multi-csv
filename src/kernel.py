65ec42c06019337e47f5fa23f4db617aef526cfa
WyattHo <wyatthoho@gmail.com>
2023-08-29 11:21:02 +0800

    feat: config filednames. style: clean code

--- src/kernel.py
+++ src/kernel.py
@@ -15,8 +15,7 @@ def read_configurations(config_name: str):
 
 
 def collect_csvs_from_directory(data_dir: str) -> Sequence[str]:
-    path_obj = Path(data_dir)
-    csvs = list(path_obj.glob('*.csv'))
+    csvs = list(Path(data_dir).glob('*.csv'))
     return csvs
 
 
@@ -26,40 +25,38 @@ def initialize_figure(figsize: Sequence[float]) -> Tuple[plt.Figure, plt.Axes]:
     return fig, ax
 
 
-def plot_data(ax: plt.Axes, csvs: Sequence[str], labels: Sequence[str]):
-    for csv, label in zip(csvs, labels):
-        path = str(csv)
-        df = pd.read_csv(path)
-        ax.semilogx(df['frequency'], df['max_stress'], label=label)
+def plot_data(
+        ax: plt.Axes, csvs: Sequence[str], labels: Sequence[str],
+        fieldnames: Sequence[Dict[str, str]]):
+    for csv, fieldname, label in zip(csvs, fieldnames, labels):
+        df = pd.read_csv(csv)
+        values_x = df[fieldname['x']]
+        values_y = df[fieldname['y']]
+        ax.semilogx(values_x, values_y, label=label)
 
 
 def set_axes(ax: plt.Axes, misc_config: Dict):
-    if misc_config['title']:
-        ax.set_title(misc_config['title'])
-    if misc_config['xlabel']:
-        ax.set_xlabel(misc_config['xlabel'])
-    if misc_config['ylabel']:
-        ax.set_ylabel(misc_config['ylabel'])
-    if misc_config['xlim']:
-        ax.set_xlim(misc_config['xlim'])
-    if misc_config['ylim']:
-        ax.set_ylim(misc_config['ylim'])
-    if misc_config['grid_visible']:
-        ax.grid(visible=True, axis='both')
+    ax.set_title(misc_config.get('title', ''))
+    ax.set_xlabel(misc_config.get('xlabel', ''))
+    ax.set_ylabel(misc_config.get('ylabel', ''))
+    ax.set_xlim(misc_config.get('xlim'))
+    ax.set_ylim(misc_config.get('ylim'))
+    ax.grid(visible=misc_config.get('grid_visible'), axis='both')
     if misc_config['legend_visible']:
         ax.legend()
 
 
 def main(config_name: str = 'config.json'):
     config = read_configurations(config_name)
-    DATA_DIR = config['plot']['data_dir']
-    FIG_SIZE = config['plot']['figsize']
-    LABELS = config['plot']['labels']
+    data_dir = config['plot']['data_dir']
+    figsize = config['plot']['figsize']
+    labels = config['plot']['labels']
+    fieldnames = config['plot']['fieldnames']
     misc_config = config['plot']['misc']
 
-    csvs = collect_csvs_from_directory(DATA_DIR)
-    fig, ax = initialize_figure(FIG_SIZE)
-    plot_data(ax, csvs, LABELS)
+    csvs = collect_csvs_from_directory(data_dir)
+    fig, ax = initialize_figure(figsize)
+    plot_data(ax, csvs, labels, fieldnames)
     set_axes(ax, misc_config)
     plt.show()
 
