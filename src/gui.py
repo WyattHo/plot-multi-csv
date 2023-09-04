c2291de71e4fc3d85eb086b32d53ebe942588417
WyattHo <wyatthoho@gmail.com>
2023-09-04 13:38:10 +0800

    feat: make frames resizable

--- src/gui.py
+++ src/gui.py
@@ -56,43 +56,45 @@ def create_directory_frame(
     frame.grid(row=0, column=0, sticky=tk.NSEW, **PADS)
     frame['font'] = font_label
 
-    subframe_left = tk.Frame(frame)
-    subframe_left.grid(row=0, column=0)
-    subframe_right = tk.Frame(frame)
-    subframe_right.grid(row=0, column=1, rowspan=2)
+    frame.rowconfigure(0, weight=1)
+    frame.columnconfigure(0, weight=1)
+    frame.columnconfigure(1, weight=1)
 
-    table = create_directory_table(subframe_left)
+    subframe_directory = tk.Frame(frame)
+    subframe_action = tk.Frame(frame)
+
+    subframe_directory.grid(row=0, column=0)
+    subframe_action.grid(row=0, column=1)
+
+    treeview = create_directory_table(subframe_directory)
     button = tk.Button(
-        subframe_right,
+        subframe_action,
         text='Choose',
-        command=lambda: kernel.open_files(table),
+        command=lambda: kernel.open_files(treeview),
         width=6
     )
     button.grid(row=0, column=0, **PADS)
     button['font'] = font_btn
-    return table
+    return treeview
 
 
 def create_working_subframes(frame: tk.LabelFrame) -> Tuple[tk.Frame]:
-    subframe_left_1 = tk.Frame(frame, width=250, height=300)
-    subframe_left_1.grid(row=0, column=0, **PADS)
-    subframe_left_1.propagate(0)
+    subframe_data = tk.Frame(frame, width=250, height=300)
+    subframe_data.grid(row=0, column=0, **PADS)
+    subframe_data.propagate(0)
 
-    subframe_left_2 = tk.Frame(frame)
-    subframe_left_2.grid(row=1, column=0)
-    subframe_left_2.propagate(0)
+    subframe_data_action = tk.Frame(frame)
+    subframe_data_action.grid(row=1, column=0)
+    subframe_data_action.propagate(0)
 
-    subframe_right_1 = tk.Frame(frame, width=250, height=300)
-    subframe_right_1.grid(row=0, column=1, **PADS)
-    subframe_right_1.propagate(0)
+    subframe_plot = tk.Frame(frame, width=250, height=300)
+    subframe_plot.grid(row=0, column=1, **PADS)
+    subframe_plot.propagate(0)
 
-    subframe_right_2 = tk.Frame(frame)
-    subframe_right_2.grid(row=1, column=1)
-    subframe_right_2.propagate(0)
-    return subframe_left_1, subframe_left_2, subframe_right_1, subframe_right_2
+    return subframe_data, subframe_data_action, subframe_plot
 
 
-def fill_subframe_left_1(subframe: tk.Frame) -> ttk.Treeview:
+def fill_subframe_data(subframe: tk.Frame) -> ttk.Treeview:
     scrollbar_ver = tk.Scrollbar(subframe)
     scrollbar_ver.pack(side=tk.RIGHT, fill=tk.Y)
     scrollbar_hor = tk.Scrollbar(subframe, orient='horizontal')
@@ -115,7 +117,7 @@ def fill_subframe_left_1(subframe: tk.Frame) -> ttk.Treeview:
     return treeview
 
 
-def fill_subframe_left_2(
+def fill_subframe_data_action(
         subframe: tk.Frame, treeview_data: ttk.Treeview,
         treeview_filenames: ttk.Treeview, font_btn: font.Font):
 
@@ -142,45 +144,15 @@ def fill_subframe_left_2(
     clear_btn['font'] = font_btn
 
 
-def fill_subframe_right_1(subframe: tk.Frame):
-    scrollbar_ver = tk.Scrollbar(subframe)
-    scrollbar_ver.pack(side=tk.RIGHT, fill=tk.Y)
-    scrollbar_hor = tk.Scrollbar(subframe, orient='horizontal')
-    scrollbar_hor.pack(side=tk.BOTTOM, fill=tk.X)
-
-    treeview = ttk.Treeview(
-        subframe,
-        yscrollcommand=scrollbar_ver.set,
-        xscrollcommand=scrollbar_hor.set,
-        height=15
-    )
-    treeview.pack(fill='both')
-    treeview.propagate(0)
-    scrollbar_ver.config(command=treeview.yview)
-    scrollbar_hor.config(command=treeview.xview)
-    treeview['columns'] = ('1', )
-    treeview['show'] = 'headings'
-    treeview.column('1')
-    treeview.heading('1', text='')
-
-
-def fill_subframe_right_2(subframe: tk.Frame, font_btn: font.Font):
-    draw_btn = tk.Button(
+def fill_subframe_plot(subframe: tk.Frame, font_btn: font.Font):
+    button = tk.Button(
         subframe,
         text='Draw',
         command=lambda: kernel.draw(),
         width=6
     )
-    clear_btn = tk.Button(
-        subframe,
-        text='Clear',
-        command=lambda: kernel.clear(),
-        width=6
-    )
-    draw_btn.grid(row=0, column=0, **PADS)
-    clear_btn.grid(row=0, column=1, **PADS)
-    draw_btn['font'] = font_btn
-    clear_btn['font'] = font_btn
+    button.grid(row=0, column=0, **PADS)
+    button['font'] = font_btn
 
 
 def create_working_frame(
@@ -189,29 +161,31 @@ def create_working_frame(
 
     frame = tk.LabelFrame(root, text='Working area')
     frame.grid(row=1, column=0, sticky=tk.NSEW, **PADS)
-    frame.propagate(0)
     frame['font'] = font_label
 
-    subframe_left_1, subframe_left_2, subframe_right_1, subframe_right_2\
+    frame.rowconfigure(0, weight=1)
+    frame.columnconfigure(0, weight=1)
+    frame.columnconfigure(1, weight=1)
+
+    subframe_data, subframe_data_action, subframe_plot\
         = create_working_subframes(frame)
 
-    treeview_data = fill_subframe_left_1(subframe_left_1)
-    fill_subframe_left_2(
-        subframe_left_2,
+    treeview_data = fill_subframe_data(subframe_data)
+    fill_subframe_data_action(
+        subframe_data_action,
         treeview_data,
         treeview_filenames,
         font_btn
     )
-    fill_subframe_right_1(subframe_right_1)
-    fill_subframe_right_2(subframe_right_2, font_btn)
+    fill_subframe_plot(subframe_plot, font_btn)
 
 
 def main():
     root = initial_main_window()
     font_label = font.Font(family='Helvetica', size=10)
     font_btn = font.Font(family='Helvetica', size=10)
-    directory_table = create_directory_frame(root, font_label, font_btn)
-    create_working_frame(root, font_label, font_btn, directory_table)
+    treeview_filenames = create_directory_frame(root, font_label, font_btn)
+    create_working_frame(root, font_label, font_btn, treeview_filenames)
     root.mainloop()
 
 
