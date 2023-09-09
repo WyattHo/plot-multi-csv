3e4076103892f6c9374ca092096db8acba6a5676
Wyatt Ho <wyatthoho@gmail.com>
2023-09-09 14:51:37 +0800

    feat: make the gui become OOP style

--- src/gui.py
+++ src/gui.py
@@ -5,150 +5,135 @@ from tkinter import ttk
 import kernel
 
 
-PADS = {
-    'padx': 5,
-    'pady': 5,
-    'ipadx': 1,
-    'ipady': 1,
-}
-ROOT_MINSIZE = {
-    'width': 400,
-    'height': 400
-}
+class MyApp:
+    PADS = {
+        'padx': 5, 'pady': 5,
+        'ipadx': 1, 'ipady': 1,
+    }
+    ROOT_MINSIZE = {
+        'width': 400, 'height': 400
+    }
 
+    def __init__(self):
+        self.root = self.initial_main_window()
+        self.font_label = font.Font(family='Helvetica', size=10)
+        self.font_btn = font.Font(family='Helvetica', size=10)
+        self.treeview_filenames = self.create_directory_frame()
+        self.notebook_data = self.create_data_frame()
+        self.create_curve_frame()
+        self.create_axes_frame()
+        self.root.mainloop()
 
-def initial_main_window() -> tk.Tk:
-    root = tk.Tk()
-    root.title('PlotCSV')
-    root.columnconfigure(0, weight=1)
-    root.columnconfigure(1, weight=1)
-    root.rowconfigure(0, weight=1)
-    root.rowconfigure(1, weight=5)
-    root.state('zoomed')
-    root.minsize(**ROOT_MINSIZE)
-    root.configure()
-    return root
+    def initial_main_window(self) -> tk.Tk:
+        root = tk.Tk()
+        root.title('PlotCSV')
+        root.columnconfigure(0, weight=1)
+        root.columnconfigure(1, weight=1)
+        root.rowconfigure(0, weight=1)
+        root.rowconfigure(1, weight=5)
+        root.state('zoomed')
+        root.minsize(**self.ROOT_MINSIZE)
+        root.configure()
+        return root
 
+    def create_directory_frame(self) -> ttk.Treeview:
+        frame = tk.LabelFrame(self.root, text='Choose csv files')
+        frame.grid(row=0, column=0, columnspan=3, sticky=tk.NSEW, **self.PADS)
+        frame['font'] = self.font_label
 
-def create_directory_frame(
-        root: tk.Tk, font_label: font.Font,
-        font_btn: font.Font) -> ttk.Treeview:
+        frame.rowconfigure(0, weight=1)
+        frame.columnconfigure(0, weight=1)
 
-    frame = tk.LabelFrame(root, text='Choose csv files')
-    frame.grid(row=0, column=0, columnspan=3, sticky=tk.NSEW, **PADS)
-    frame['font'] = font_label
+        subframe_directory = tk.Frame(frame)
+        subframe_directory.grid(row=0, column=0, sticky=tk.NSEW)
 
-    frame.rowconfigure(0, weight=1)
-    frame.columnconfigure(0, weight=1)
+        subframe_action = tk.Frame(frame)
+        subframe_action.grid(row=0, column=1)
 
-    subframe_directory = tk.Frame(frame)
-    subframe_directory.grid(row=0, column=0, sticky=tk.NSEW)
-
-    subframe_action = tk.Frame(frame)
-    subframe_action.grid(row=0, column=1)
-
-    columns = ('CSV ID', 'CSV Path')
-    treeview = kernel.create_treeview(subframe_directory, columns, 5)
-    button = tk.Button(
-        subframe_action,
-        text='Choose',
-        command=lambda: kernel.open_files(treeview),
-        width=6
-    )
-    button.grid(row=0, column=0, **PADS)
-    button['font'] = font_btn
-    return treeview
-
-
-def create_data_frame(
-        root: tk.Tk, font_label: font.Font,
-        font_btn: font.Font, treeview_filenames: ttk.Treeview):
-
-    frame = tk.LabelFrame(root, text='Review data')
-    frame.grid(row=1, column=0, sticky=tk.NSEW, **PADS)
-    frame['font'] = font_label
-
-    frame.rowconfigure(0, weight=1)
-    frame.columnconfigure(0, weight=1)
-    frame.columnconfigure(1, weight=1)
-
-    notebook = ttk.Notebook(frame)
-    notebook.grid(row=0, column=0, columnspan=2, sticky=tk.NSEW)
-    kernel.initial_tabs_with_treeview(notebook)
-
-    import_btn = tk.Button(
-        frame,
-        text='Import',
-        command=lambda: kernel.import_csv(
-            treeview_filenames,
-            notebook
-        ),
-        width=6
-    )
-    import_btn.grid(row=1, column=0, **PADS)
-    import_btn['font'] = font_btn
-
-    clear_btn = tk.Button(
-        frame,
-        text='Clear',
-        command=lambda: kernel.initial_tabs_with_treeview(notebook),
-        width=6
-    )
-    clear_btn.grid(row=1, column=1, **PADS)
-    clear_btn['font'] = font_btn
-    return notebook
-
-
-def create_curve_frame(root: tk.Tk, notebook_data: ttk.Notebook):
-    frame = tk.LabelFrame(root, text='Curve settings')
-    frame.grid(row=1, column=1, sticky=tk.NSEW, **PADS)
-
-    frame.rowconfigure(1, weight=1)
-    frame.columnconfigure(0, weight=1)
-    frame.columnconfigure(1, weight=1)
-
-    notebook_curve = ttk.Notebook(frame)
-    notebook_curve.grid(row=1, column=0, columnspan=2, sticky=tk.NSEW)
-    kernel.initial_tabs_with_frame(notebook_curve)
-
-    label = tk.Label(frame, text='Curve numbers')
-    label.grid(row=0, column=0, **PADS)
-
-    spinbox = tk.Spinbox(
-        frame, from_=1, to=20, width=3,
-        command=lambda: kernel.create_curve_tab(
-            notebook_data,
-            notebook_curve,
-            spinbox
+        columns = ('CSV ID', 'CSV Path')
+        treeview = kernel.create_treeview(subframe_directory, columns, 5)
+        button = tk.Button(
+            subframe_action,
+            text='Choose',
+            command=lambda: kernel.open_files(treeview),
+            width=6
         )
-    )
-    spinbox.grid(row=0, column=1, **PADS)
+        button.grid(row=0, column=0, **self.PADS)
+        button['font'] = self.font_btn
+        return treeview
 
+    def create_data_frame(self):
+        frame = tk.LabelFrame(self.root, text='Review data')
+        frame.grid(row=1, column=0, sticky=tk.NSEW, **self.PADS)
+        frame['font'] = self.font_label
 
-def create_axes_frame(root: tk.Tk, font_btn: font.Font):
-    frame = tk.LabelFrame(root, text='Axes settings')
-    frame.grid(row=1, column=2, sticky=tk.NSEW, **PADS)
-    button = tk.Button(
-        frame,
-        text='Draw',
-        command=lambda: kernel.draw(),
-        width=6
-    )
-    button.grid(row=0, column=0, **PADS)
-    button['font'] = font_btn
+        frame.rowconfigure(0, weight=1)
+        frame.columnconfigure(0, weight=1)
+        frame.columnconfigure(1, weight=1)
 
+        notebook = ttk.Notebook(frame)
+        notebook.grid(row=0, column=0, columnspan=2, sticky=tk.NSEW)
+        kernel.initial_tabs_with_treeview(notebook)
 
-def main():
-    root = initial_main_window()
-    font_label = font.Font(family='Helvetica', size=10)
-    font_btn = font.Font(family='Helvetica', size=10)
-    treeview_filenames = create_directory_frame(root, font_label, font_btn)
-    notebook_data = create_data_frame(
-        root, font_label, font_btn, treeview_filenames)
-    create_curve_frame(root, notebook_data)
-    create_axes_frame(root, font_btn)
-    root.mainloop()
+        import_btn = tk.Button(
+            frame,
+            text='Import',
+            command=lambda: kernel.import_csv(
+                self.treeview_filenames,
+                notebook
+            ),
+            width=6
+        )
+        import_btn.grid(row=1, column=0, **self.PADS)
+        import_btn['font'] = self.font_btn
+
+        clear_btn = tk.Button(
+            frame,
+            text='Clear',
+            command=lambda: kernel.initial_tabs_with_treeview(notebook),
+            width=6
+        )
+        clear_btn.grid(row=1, column=1, **self.PADS)
+        clear_btn['font'] = self.font_btn
+        return notebook
+
+    def create_curve_frame(self):
+        frame = tk.LabelFrame(self.root, text='Curve settings')
+        frame.grid(row=1, column=1, sticky=tk.NSEW, **self.PADS)
+
+        frame.rowconfigure(1, weight=1)
+        frame.columnconfigure(0, weight=1)
+        frame.columnconfigure(1, weight=1)
+
+        notebook_curve = ttk.Notebook(frame)
+        notebook_curve.grid(row=1, column=0, columnspan=2, sticky=tk.NSEW)
+        kernel.initial_tabs_with_frame(notebook_curve)
+
+        label = tk.Label(frame, text='Curve numbers')
+        label.grid(row=0, column=0, **self.PADS)
+
+        spinbox = tk.Spinbox(
+            frame, from_=1, to=20, width=3,
+            command=lambda: kernel.create_curve_tab(
+                self.notebook_data,
+                notebook_curve,
+                spinbox
+            )
+        )
+        spinbox.grid(row=0, column=1, **self.PADS)
+
+    def create_axes_frame(self):
+        frame = tk.LabelFrame(self.root, text='Axes settings')
+        frame.grid(row=1, column=2, sticky=tk.NSEW, **self.PADS)
+        button = tk.Button(
+            frame,
+            text='Draw',
+            command=lambda: kernel.draw(),
+            width=6
+        )
+        button.grid(row=0, column=0, **self.PADS)
+        button['font'] = self.font_btn
 
 
 if __name__ == '__main__':
-    main()
+    MyApp()
